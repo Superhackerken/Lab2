f1039910fc1b98e79705a570e113d3272272b6e8
superken <CKENDRICSES06.24@ichat.sp.edu.sg>
2025-05-08 15:33:13 +0800

    Changed BMI file for Lab3

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(weight, height):
     print("BMI = " + str(bmi))
     if (bmi < 18.5):
         print("User is under weight!")
+        return -1
     elif (bmi <= 25): 
         print("User has normal weight!")
+        return 0
     else:
         print("User is over weight!!!")
+        return 1
 
 calculate_bmi(90, 1.73)
