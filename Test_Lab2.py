919e8d2e55c3c3378f7add28a7ce6b3bb703f5be
superken <CKENDRICSES06.24@ichat.sp.edu.sg>
2025-05-16 16:59:50 +0800

    Added a test to test some lab 2 functions!!

---
+++ Test_Lab2.py
@@ -0,0 +1,28 @@
+import Lab2
+
+def test_min_max():
+    result = []
+    input_arr = [60, 40, 30, 20]
+    test_arr = [20, 60]
+
+    result = Lab2.find_min_max(input_arr)
+
+    assert (result == test_arr)
+
+def test_calc_average():
+    result = []
+    input_arr = [5, 67, 32, 50, 30]
+    test_arr = 36.8
+
+    result = Lab2.calc_average(input_arr)
+
+    assert (result == test_arr)
+
+def test_calc_median_temperature():
+    result = []
+    input_arr = [9.0, 16.0, 24.0, 30.0]
+    test_arr = 20
+
+    result = Lab2.calc_median_temperature(input_arr)
+
+    assert (result == test_arr)
